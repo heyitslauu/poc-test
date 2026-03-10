81117d1cafea919f5725d31032b66653b4405e93
Laurence Cadag <laurencecadag1@gmail.com>
2026-03-10 09:19:21 +0800

    Refactor database configuration to enhance SSL handling and improve code clarity

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -6,16 +6,22 @@ export interface DatabaseConfig {
   user: string;
   password: string;
   database: string;
-  ssl: boolean;
+  ssl: boolean | { rejectUnauthorized: boolean };
 }
 
 export const DATABASE_CONNECTION = 'database_connection';
 
-export const getDatabaseConfig = (configService: ConfigService<Record<string, string>>): DatabaseConfig => ({
-  host: configService.getOrThrow('POSTGRES_HOST'),
-  port: parseInt(configService.getOrThrow('POSTGRES_PORT'), 10),
-  user: configService.getOrThrow('POSTGRES_USER'),
-  password: configService.getOrThrow('POSTGRES_PASSWORD'),
-  database: configService.getOrThrow('POSTGRES_DB'),
-  ssl: configService.get('POSTGRES_SSL') !== 'false',
-});
+export const getDatabaseConfig = (configService: ConfigService<Record<string, string>>): DatabaseConfig => {
+  const isProduction = process.env.NODE_ENV === 'production';
+  const envSsl = configService.get('POSTGRES_SSL');
+
+  const ssl = envSsl ? { rejectUnauthorized: false } : isProduction ? { rejectUnauthorized: false } : false;
+  return {
+    host: configService.getOrThrow('POSTGRES_HOST'),
+    port: parseInt(configService.getOrThrow('POSTGRES_PORT'), 10),
+    user: configService.getOrThrow('POSTGRES_USER'),
+    password: configService.getOrThrow('POSTGRES_PASSWORD'),
+    database: configService.getOrThrow('POSTGRES_DB'),
+    ssl: ssl,
+  };
+};
